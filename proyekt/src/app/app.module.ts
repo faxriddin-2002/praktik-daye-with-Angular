c5c3cc131369168cf88ca9ca40d75fc20cddec8f
unknown <sodiqofaxriddin6@gmail.com>
2021-06-08 09:22:43 +0500

    new file

--- proyekt/src/app/app.module.ts
+++ proyekt/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSectionComponent
   ],
   imports: [
     BrowserModule,
